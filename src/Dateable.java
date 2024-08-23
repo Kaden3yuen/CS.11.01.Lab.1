ff274fa53b0e1f667d4f6a7500a0e1996bf7dfbc
yuenkaden <kadeny2027@student.cis.edu.hk>
2024-08-23 12:46:55 +0800

    Lab 1 completed

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your age: ");
+        int age = scanner.nextInt();
+        int DatingAge = 7 + (age / 2);
+
+        System.out.println(age + "should date somebody who is at least " + DatingAge + " old");
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your age: ");
+        int age = scanner.nextInt();
+        int ReadingAge = 100 - age;
+
+        System.out.println(age + " year olds should read at least " + ReadingAge + " pages before giving up on a book.");
 
     }
 }
